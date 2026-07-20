7ce2d0685a7df7979734bb3ab1c49092d1cfd3a0
Mark Johnson <22575680+cazanson@users.noreply.github.com>
2026-07-20 11:23:57 -0500

    Trailing slash issues test

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ import { remarkObsidian } from './src/plugins/remark-obsidian.ts';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cazanson.github.io',
+  site: 'https://cazanson.github.io/',
 
   image: {
     responsiveStyles: true,
